974f55d880bfc0923810a4bb7f5d750310636b4c
LaxirMods <muhammadrizalasri2@gmail.com>
2022-10-23 02:19:11 +0800

    new era

---
+++ plugins/creator2.js
@@ -0,0 +1,37 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn, usedPrefix, text, args, command }) => {
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
+let name = await conn.getName(who)
+
+
+if (command == 'whmods1') {
+ let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;Saya Owner LaxirMods;Bot;;Md\nFN:Saya Owner LazirBot WhatsApp, Md\nNICKNAME:👑 Owner LazirBot\nORG:👤 OWNER 🤖\nTITLE:MODS\nitem1.TEL;waid=62853329039348:+62 853-3203-9348\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET:laxirmods@gmail.com\nitem3.X-ABLabel:💌 Mail Owner LazirBOT\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 KEPO LU YAH\nEND:VCARD`
+const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fakes })
+let caption = `👋 Hai *${name} @${who.split("@")[0]}*, Nih Owner *${conn.user.name}* kak`
+    await conn.sendButton(m.chat, caption, author, null, [['🎀 Sapa Owner', 'Huuu']], m, { quoted: tag_own, mentions: conn.parseMention(caption) })
+}
+if (command == 'whmods2') {
+  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;${author};;;\nFN:${author}\nORG:${author}\nTITLE:\nitem1.TEL;waid=62853329039348:+62 853-3203-9348\nitem1.X-ABLabel:${author}\nX-WA-BIZ-DESCRIPTION:${htjava} Nih pengembang ku kack yg mengaktifkan aq.\nX-WA-BIZ-NAME:${author}\nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fakes })
+}
+if (command == 'whmods3') {
+  try {
+  const sentMsg = await conn.sendContactArray(m.chat, [
+    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `👑 Developer Bot `, `🚫 Don't call me 🥺`, `laxirmods@gmail.com`, `🇮🇩 Indonesia`, `🚀 https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow`, `👤 Gada pawang nih senggol dong 😔`],
+    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🔥 Bot WhatsApp 🐣`, `📵 Don't spam/call me 😢`, `Nothing`, `🇮🇩 Indonesia`, `🚀 https://github.com/LaxirMods/`, `🤖 Hanya bot biasa yang kadang suka eror ☺`]
+  ], fakes)
+  await conn.reply(m.chat, `Halo kak @${m.sender.split(`@`)[0]} itu nomor ownerku , jangan di spam ya ka😉`, sentMsg, {mentions: [m.sender]})
+  } catch {
+  const sentMsg = await conn.sendContact(m.chat, `${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, m)
+  await conn.reply(m.chat, `Halo kak @${m.sender.split(`@`)[0]} itu nomor team developerku, jangan di apa-apain ya kak😖`, sentMsg, {mentions: [m.sender]})
+  }
+  }
+
+}
+handler.help = ['whmods1', 'whmods2', 'whmods3']
+handler.tags = ['info']
+
+handler.command = /^(whmods1|whmods2|whmods3)$/i
+
+export default handler
---
+++ plugins/info-script.js
@@ -0,0 +1,21 @@
+import fetch from 'node-fetch'
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
+
+let str = `*https://github.com/LaxirMods*`
+let wibu = `https://hadi-api.herokuapp.com/api/loli` 
+let thumb = await(await fetch(wibu)).buffer()
+conn.sendButtonDoc(m.chat, str, wm,'ᴏᴋ','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://Instagram.com/drizasri",
+    title: 'new-era',
+    body: 'want source code?',
+    thumbnail: thumb,
+    sourceUrl: sig
+  }
+  } }) 
+          }
+handler.help = ['sc', 'script']
+handler.tags = ['info', 'main']
+handler.command =  /^(script|sc)$/i
+
+export default handler
